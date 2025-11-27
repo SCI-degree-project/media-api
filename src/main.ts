07e882c537063f3e1c1f6c75a17b44a7bdbffdcd
Leonardo Herrera <leonardoherrera2304@gmail.com>
2025-11-27 01:46:38 -0400

    feat: media endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(bodyParser.json({ limit: '25mb' }));
+  app.use(bodyParser.urlencoded({ limit: '25mb', extended: true }));
+
   await app.listen(3000);
 }
 bootstrap();
